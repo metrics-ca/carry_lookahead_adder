073b1273ed17b687e3806e93597602d3e101dd40
Benjamin Chin <benjamin.chin@metrics.ca>
2021-08-24 17:41:15 -0400

    Rename *selfchecking_tb.v to *directed_tb.v, based on Aimee's mod, and added variable checker_result

--- SystemVerilog/testbench/carry_lookahead_adder_directed_tb.v
+++ SystemVerilog/testbench/carry_lookahead_adder_directed_tb.v
@@ -3,8 +3,6 @@
 // Added Self Checking Capability to the testbench
 ///////////////////////////////////////////////////////////////////////////////
 
-//`include "carry_lookahead_adder.v"
-
 module carry_lookahead_adder_tb ();
 
   parameter WIDTH = 3;
@@ -12,6 +10,7 @@ module carry_lookahead_adder_tb ();
   reg [WIDTH-1:0] r_ADD_1 = 0;
   reg [WIDTH-1:0] r_ADD_2 = 0;
   wire [WIDTH:0]  w_RESULT;
+  integer checker_result;
 
   reg clk = 0;
   integer error_cnt = 0;
@@ -34,12 +33,13 @@ module carry_lookahead_adder_tb ();
 
   always @(posedge clk)
       begin
-          if(w_RESULT == (r_ADD_1 + r_ADD_2)) begin
-              $display("[time] [ModuleName/Checker] Info: Sum is correct: r_add1, r_add2, result");
+          checker_result = r_ADD_1 + r_ADD_2;
+          if(w_RESULT == checker_result) begin
+              $display("[%0t ns] [%m/Checker] Info: Sum is correct: r_add1=%0d, r_add2=%0d, result=%0d", $time, r_ADD_1, r_ADD_2, w_RESULT);
           end
           else
           begin
-              $display("[time] [ModuleName/Checker] Error: Sum is incorrect. r_add1, r_add2, exp_result, received result ");
+              $display("[%0t ns] [%m/Checker] Error: Sum is incorrect. r_add1=%0d, r_add2=%0d, exp_result=%0d, received result=%0d", $time, r_ADD_1, r_ADD_2,checker_result, w_RESULT );
               error_cnt = error_cnt + 1;
           end
       end
@@ -48,18 +48,21 @@ module carry_lookahead_adder_tb ();
   initial
     begin
       @(negedge clk);
-      $display("[time] [Stimulus] Info: New values r_add1, r_add2");
       r_ADD_1 = 3'b000;
       r_ADD_2 = 3'b001;
+      $display("[%0t ns] [%m/Stimulus] New values r_add1=%0d, r_add2=%0d", $time, r_ADD_1, r_ADD_2);
       @(negedge clk);
       r_ADD_1 = 3'b010;
       r_ADD_2 = 3'b010;
+      $display("[%0t ns] [%m/Stimulus] New values r_add1=%0d, r_add2=%0d", $time, r_ADD_1, r_ADD_2);
       @(negedge clk);
       r_ADD_1 = 3'b101;
       r_ADD_2 = 3'b110;
+      $display("[%0t ns] [%m/Stimulus] New values r_add1=%0d, r_add2=%0d", $time, r_ADD_1, r_ADD_2);
       @(negedge clk);
       r_ADD_1 = 3'b111;
       r_ADD_2 = 3'b111;
+      $display("[%0t ns] [%m/Stimulus] New values r_add1=%0d, r_add2=%0d", $time, r_ADD_1, r_ADD_2);
       @(negedge clk);
       if (error_cnt > 0) begin
           $display("Test: FAILED");
