0e0bfd65e8fba2dfa77063d2921765178a587991
Aimee Sutton <aimee.sutton@metrics.ca>
2021-09-21 15:35:07 -0400

    Updated stimulus to use for loops. Added -sv to dsim command line.

--- SystemVerilog/testbench/carry_lookahead_adder_directed_tb.v
+++ SystemVerilog/testbench/carry_lookahead_adder_directed_tb.v
@@ -18,7 +18,7 @@ module carry_lookahead_adder_tb ();
   carry_lookahead_adder #(.WIDTH(WIDTH)) carry_lookahead_inst
     (
      .i_add1(r_ADD_1),
-     .i_add2(r_ADD_2),
+     .i_add2(r_ADD2_2),
      .o_result(w_RESULT)
      );
 
@@ -47,23 +47,16 @@ module carry_lookahead_adder_tb ();
 
   initial
     begin
-      @(negedge clk);
-      r_ADD_1 = 3'b000;
-      r_ADD_2 = 3'b001;
-      $display("[%0t ns] [%m/Stimulus] New values r_add1=%0d, r_add2=%0d", $time, r_ADD_1, r_ADD_2);
-      @(negedge clk);
-      r_ADD_1 = 3'b010;
-      r_ADD_2 = 3'b010;
-      $display("[%0t ns] [%m/Stimulus] New values r_add1=%0d, r_add2=%0d", $time, r_ADD_1, r_ADD_2);
-      @(negedge clk);
-      r_ADD_1 = 3'b101;
-      r_ADD_2 = 3'b110;
-      $display("[%0t ns] [%m/Stimulus] New values r_add1=%0d, r_add2=%0d", $time, r_ADD_1, r_ADD_2);
-      @(negedge clk);
-      r_ADD_1 = 3'b111;
-      r_ADD_2 = 3'b111;
-      $display("[%0t ns] [%m/Stimulus] New values r_add1=%0d, r_add2=%0d", $time, r_ADD_1, r_ADD_2);
-      @(negedge clk);
+      for (int op1 = 0; op1 < 8; op1++) begin
+        for (int op2 = 0; op2 < 8; op2++) begin   
+          @(negedge clk);   
+          r_ADD_1 = op1; 
+          r_ADD_2 = op2;
+          $display("[%0t ns] [%m/Stimulus] New values r_add1=%0d, r_add2=%0d", $time, r_ADD_1, r_ADD_2);
+        end
+      end 
+      @(negedge clk); 
+      
       if (error_cnt > 0) begin
           $display("Test: FAILED");
       end
