613612c6e6ae14a06358d872c6e6b80ea9171e5e
Ronen Shoham <ronen.shoham@metrics.ca>
2022-03-18 06:26:07 -0400

    remove time from check.bash

--- SystemVerilog/sim/check.bash
+++ SystemVerilog/sim/check.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 result="Passed" && `grep -c "Test: PASSED" dsim.log > /dev/null` || result="Failed"
-echo "{ summary: $result, time : 2 }" > results.json
+echo "{ summary: $result }" > results.json
