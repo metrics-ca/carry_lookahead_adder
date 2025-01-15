ccf7bf13727f8df0840dfa28b7c0faba29d72328
Shaun Luong <shaun.luong@metrics.ca>
2025-01-15 10:34:37 -0700

    Re-added run_mdc scripts because they are needed for smoke tests.

---
+++ SystemVerilog/sim/run_mdc.ps1
@@ -0,0 +1,5 @@
+# run_mdc.ps1
+# PowerShell script to simulate design with Altair® DSim™ Cloud
+
+# Analyze, Elaborate, and Run in one step.
+mdc dsim -a '-F filelist.txt +acc+b -waves waves.mxd'
---
+++ SystemVerilog/sim/run_mdc.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+# run_mdc.sh
+# Bash script to simulate design with Altair® DSim™ Cloud
+
+# Analyze, Elaborate, and Run in one step.
+mdc dsim -a '-F filelist.txt +acc+b -waves waves.mxd'
