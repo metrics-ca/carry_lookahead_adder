bfb5146c1fe4d04c740a9a5bd52c214af3adae32
Brian Hill <bhill@altair.com>
2025-03-13 12:24:42 -0400

    Added Windows command scripts alongside PowerShell scripts

---
+++ VHDL/sim/dsim_local.bat
@@ -0,0 +1,15 @@
+@echo off
+REM dsim_local.bat
+REM Windows command script to simulate design with Altair® DSim™ on local machine
+
+REM Set up environment
+set WAVE_FILE=waves.mxd
+
+REM Compile standard libraries
+dlib rm -lib ieee
+dlib map -lib ieee "%STD_LIBS%/ieee93"
+
+REM Analyze separately, Elaborate and Run in one step.
+dvhcom -lib dut -F filelist_VHDL.txt
+dvhcom ../testbench/carry_lookahead_adder_directed_tb.vhd
+dsim -top work.carry_lookahead_adder_tb -L dut +acc+b -waves %WAVE_FILE%
