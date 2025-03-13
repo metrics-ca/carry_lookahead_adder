b20e6cf6a57a57c0186faca14dc25b3a3d09f4a0
Brian Hill <bhill@altair.com>
2025-03-13 13:34:33 -0400

    Added local command line scripts for SH interpretter

---
+++ VHDL/sim/dsim_local.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env sh
+# dsim_local.sh
+# Shell script to simulate design with Altair® DSim™ on local machine
+
+# Set up environment
+WAVE_FILE=waves.mxd
+
+# Compile standard libraries
+dlib rm -lib ieee
+dlib map -lib ieee "$STD_LIBS/ieee93"
+
+# Analyze separately, Elaborate and Run in one step.
+dvhcom -lib dut -F filelist_VHDL.txt
+dvhcom ../testbench/carry_lookahead_adder_directed_tb.vhd
+dsim -top work.carry_lookahead_adder_tb -L dut +acc+b -waves $WAVE_FILE
